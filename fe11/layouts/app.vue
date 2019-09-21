410b5120b3506b7c256c257004c41d85c4f46b4f
qc_78c5f7528718479986ed33564243bbb3 <qc_78c5f7528718479986ed33564243bbb3@git.code.tencent.com>
2019-09-21 08:02:45 +0800

    初始化

---
+++ fe11/layouts/app.vue
@@ -0,0 +1,11 @@
+<template>
+  
+  <div>
+    <h1>标题头部</h1>
+
+    <nuxt />
+
+    <h2>底部</h2>
+
+  </div>
+</template>
---
+++ fe11/pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <h1>开课吧</h1>
+
+    <div>
+      <nuxt-link to="/register">注册</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  // layout:'app',
+  async mounted(){
+
+    // let ret = await this.$axios.get('/api/demoinfo')
+    let ret = await this.$http.get('/demoinfo')
+    console.log(ret.data)
+  },
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
